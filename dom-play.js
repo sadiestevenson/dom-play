057cc766721801a7e4d2d623dc736daf08cf2ff8
sadiestevenson <93157504+sadiestevenson@users.noreply.github.com>
2022-07-14 09:37:57 -0700

    Update dom-play.js

--- dom-play.js
+++ dom-play.js
@@ -1,3 +1,14 @@
 //alert ("HI");
 
-document.querySelectorAll()
+let actors = document.querySelectorAll(".hamlet");
+
+console.log(actors);
+
+for (let i = 0; i < actors.length; i++) {
+
+actors [i] .style.backgroundColor ="yellow";
+
+//document.querySelector(".hamlet").style.backgroundColor = "yellow";
+
+
+
