773ea07e0b2463249efc770eb303289d6ea85092
sadiestevenson <93157504+sadiestevenson@users.noreply.github.com>
2022-07-21 18:24:08 -0700

    Update dom-play.js

--- dom-play.js
+++ dom-play.js
@@ -1,14 +1,45 @@
 //alert ("HI");
 
-let actors = document.querySelectorAll(".hamlet");
+function myAlert(){
 
-console.log(actors);
+    alert("clicked");
+    
+    }
 
-for (let i = 0; i < actors.length; i++) {
+const span  = document.querySelectorAll("#play span" );
 
-actors [i] .style.backgroundColor ="yellow";
+console.log(spans);
 
-//document.querySelector(".hamlet").style.backgroundColor = "yellow";
+for (const mySpan of spans) {
+//mySpan.addEventListener("click",myAlert);
+//alert(mySpan.dataset.actor);
+mySpan.addEventListener("click",function(ev){
+   // alert("THe current actor is" + mySpan.dataset.actor);
+highlighActor(mySpan.datase.actor);
+});
+}
+
+
+
+
+
+
+
+
+
+function highlighActor (actor) {
+
+    for(const mySpan of spans) {
+
+        if(actor== mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor=='yellow';
+        }else{//other actor, bakcground white
+            mySpan.style.backgroundColor=='white';
+        }
+    }
+    
+}
+
 
 
 
